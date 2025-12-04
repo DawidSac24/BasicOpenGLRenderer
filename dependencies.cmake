410203e6c4321d61cfbb3bd36eb0580255903d8e
Dawid Sac <dawidsac24@gmail.com>
2025-12-04 16:59:55 +0100

    build: installing glm in dependencies.cmake

--- dependencies.cmake
+++ dependencies.cmake
@@ -4,10 +4,16 @@ include(FetchContent)
 FetchContent_Declare(glfw GIT_REPOSITORY https://github.com/glfw/glfw.git GIT_TAG 3.3.8)
 FetchContent_MakeAvailable(glfw)
 
-# # GLM
-# FetchContent_Declare(glm GIT_REPOSITORY https://github.com/g-truc/glm.git GIT_TAG 0.9.9.8)
-# FetchContent_MakeAvailable(glm)
-#
+include(FetchContent)
+
+FetchContent_Declare(
+	glm
+	GIT_REPOSITORY	https://github.com/g-truc/glm.git
+	GIT_TAG 	0af55ccecd98d4e5a8d1fad7de25ba429d60e863 #refs/tags/1.0.1
+)
+
+FetchContent_MakeAvailable(glm)
+
 # # GLAD
 # FetchContent_Declare(glad GIT_REPOSITORY https://github.com/Dav1dde/glad.git GIT_TAG v0.1.36)
 # FetchContent_MakeAvailable(glad)
